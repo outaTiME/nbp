9c784fe37709c930af8c3451b685201d610a6fcf
outaTiME <afalduto@gmail.com>
2012-10-15 23:45:45 -0300

    Initial commit.

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+
+var
+
+  // basic
+  express = require('express'),
+  http = require('http'),
+  path = require('path'),
+
+  // routes
+  routes = require('./routes'),
+  // user = require('./routes/user'),
+
+  // HTML5 Boilerplate
+  h5bp = require('h5bp');
+
+var app = express();
+
+app.configure(function () {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.favicon(path.join(__dirname, "public", 'favicon.ico')));
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware({
+    src: path.join(__dirname),
+    dest: path.join(__dirname, "public"),
+    compress: true
+  }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  // bp
+  app.use(h5bp.ieEdgeChromeFrameHeader());
+  app.use(h5bp.protectDotfiles());
+  app.use(h5bp.blockBackupFiles());
+  app.use(h5bp.removePoweredBy());
+  app.use(h5bp.crossDomainRules());
+  app.use(h5bp.suppressWww(true));
+  app.use(h5bp.removeEtag());
+});
+
+app.configure('development', function () {
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+// app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
+});
